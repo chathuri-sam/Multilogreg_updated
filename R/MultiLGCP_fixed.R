1e39a1334dc28d79659ba39e6763f24437d7b2ad
Chathuri Samarasekara <73504291+chathuri-sam@users.noreply.github.com>
2025-02-05 06:56:55 +1100

    MultiLGCP_fixed.R

--- R/MultiLGCP_fixed.R
+++ R/MultiLGCP_fixed.R
@@ -1,6 +1,34 @@
-#This code has been updated on work done by Hessellund et al. (2022)'s sim-lgcp-multi code found in  https://github.com/kristianhessellund/Multilogreg.git
-# This version is able to address any given window and multiple covariates. 
-#(whereas Hessellund et al. (2022) used a square window and a single covariate)
+#' @title Simulation of semi-parametric multivariate log Gaussian Cox processes
+#'
+#' @description Simulation of semi-parametric multivariate log Gaussian Cox processes.
+#'
+#' @param basecov Background intensity rho_0.
+#' @param covariate Optional. A simulated covariate. The covariate must be a matrix.
+#' @param betas A matrix with covariates.
+#' @param alphas Alpha parameters. Must be a matrix, where the number of rows correspond
+#' to the number of components in the LGCP. The number of columns correspond to the number
+#' of common latent field.
+#' @param xis Correlation scale parameters for each common random field. The correlation functions
+#' for the common latent fields are exponential.
+#' @param sigmas Sigma parameters. The number of sigma parameters must correspond
+#' to the number of components in the LGCP.
+#' @param phis Correlation scale parameters for each type-specific random field. The correlation functions
+#' for the type-specific random fields are exponential.
+#' @param n.window window size.
+#' @param n.points Expected number of point for each component in the LGCP.
+#' The length of n.points must correspond to the number of components.
+#' @param beta0s Intercepts. The length of beta0s must correspond to the number of components
+#' in the LGCP.
+#' @importFrom RandomFields RFsimulate
+#'
+#' @return Multivariate LGCP
+#' @author Kristian Bjørn Hessellund, Ganggang Xu, Yongtao Guan and Rasmus Waagepetersen.
+#' @references Hessellund, K. B., Xu, G., Guan, Y. and Waagepetersen, R. (2020)
+#' Second order semi-parametric inference for multivariate log Gaussian Cox processes.
+#'
+#'This code has been updated on work done by Hessellund et al. (2022)'s sim-lgcp-multi code found in  https://github.com/kristianhessellund/Multilogreg.git
+#'This version is able to address any given window and multiple covariates. 
+#'(whereas Hessellund et al. (2022) used a square window and a single covariate)
 
 sim_lgcp_multi_fixed <- function(basecov,covariate,betas,alphas,xis,sigmas,phis, n.window,n.points,beta0s=NULL){
   
